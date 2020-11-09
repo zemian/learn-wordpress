88f5d50c479d8d376257172491955320a4aa45df
Zemian Deng <zemiandeng@gmail.com>
2020-11-09 17:48:50 -0500

    Remove dup shortcode demos

--- wordpress/wp-content/plugins/zplugin/zplugin.php
+++ wordpress/wp-content/plugins/zplugin/zplugin.php
@@ -16,7 +16,4 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 // Bring in different test files
-require_once( dirname( __FILE__ ) . '/includes/shortcode-hello.php' );
-require_once( dirname( __FILE__ ) . '/includes/shortcode-pagelist.php' );
-require_once( dirname( __FILE__ ) . '/includes/shortcode-wpdb.php' );
 require_once( dirname( __FILE__ ) . '/bookreview-type/init.php' );
