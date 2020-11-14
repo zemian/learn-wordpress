331019057a7a0954fa950b465389990f2ceb6735
Zemian Deng <zemiandeng@gmail.com>
2020-11-14 00:23:49 -0500

    Add drop/create DB before restore

--- bin/restoredb.sh
+++ bin/restoredb.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 SCRIPT_DIR=$(dirname $0)
+echo 'DROP DATABASE wordpressdb' | mysql -u root
+echo 'CREATE DATABASE wordpressdb' | mysql -u root
 mysql -f -u root wordpressdb < $SCRIPT_DIR/../db/wordpressdb.sql
